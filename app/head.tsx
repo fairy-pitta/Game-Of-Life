b6de1f7d1a632ffba2de6688b6818e555e779fe1
Pitta <shuna120700@gmail.com>
2025-05-19 16:05:49 +0800

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <base href="/Game-Of-Life/" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Game Of Life</title>
+      </>
+    )
+  }
