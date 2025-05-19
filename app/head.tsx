6972f5578c42f6ec70f6a64a0f6bc286f0b8c247
Pitta <shuna120700@gmail.com>
2025-05-19 19:35:09 +0800

    added favicon

--- app/head.tsx
+++ app/head.tsx
@@ -5,6 +5,8 @@ export default function Head() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Game Of Life</title>
+        <meta name="description" content="Watch colorful patterns unfold in Conway's Game of Life, an interactive simulation of cellular automata." />
+        <link rel="icon" type="image/x-icon" href="/Game-Of-Life/favicon.ico" />
       </>
     )
   }
