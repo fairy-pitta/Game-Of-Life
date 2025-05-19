bd0b35fb7a6122e9d0ab4eec1b98a39c01fd49d2
Pitta <shuna120700@gmail.com>
2025-05-19 19:40:46 +0800

    update head

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,10 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Game Of Life',
-  description: 'game of life',
+  description: "Watch colorful patterns unfold in Conway's Game of Life, an interactive simulation of cellular automata.",
+  icons: {
+    icon: '/Game-Of-Life/favicon.ico',
+  },
 }
 
 export default function RootLayout({
