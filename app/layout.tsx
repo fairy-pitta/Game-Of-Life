a2ac55a4c165976edf1eb0eff219994d55c1eae6
Pitta <shuna120700@gmail.com>
2025-05-19 15:46:04 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Game Of Life',
+  description: 'game of life',
 }
 
 export default function RootLayout({
